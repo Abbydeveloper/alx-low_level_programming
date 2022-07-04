c712676f2780555a6c087c97901fc19d69d2bbe7
Abigail Adeboga <abbyboga@gmail.com>
2022-07-04 12:40:35 -0700

    Fix compile errors

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+*
+* main - Prints the alphabet in lower and uppercase using putchar
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char alp[26] = abcdefghijklmnopqrstuvwxyz\;
+int i;
+
+for (i = 0; 1 < 26; i++)
+{
+putchar(alp[i]);
+}
+for (i = 0; i < 26; i++)
+{
+putchar(toupper(alp[i]));
+}
+putchar('\n');
+return (0);
+}
