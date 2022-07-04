49092237b84210daff38d8b5683a9620760020a7
Abigail Adeboga <abbyboga@gmail.com>
2022-07-04 14:09:32 -0700

    Print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lower and uppercase using putchar
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
