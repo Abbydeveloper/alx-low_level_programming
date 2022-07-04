5f07049b2eebb166f8fbcad6f0cc35aa3741cd62
Abigail Adeboga <abbyboga@gmail.com>
2022-07-04 13:31:18 -0700

    Print  with conditions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+* main - Prints the alphabet in lower and uppercase using putchar
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+if (alp[i] != 'e' || alp[i] != 'q')
+{
+putchar(alp[i]);
+}
+}
+putchar('\n');
+return (0);
+}
