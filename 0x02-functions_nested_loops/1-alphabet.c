601bd1fb99374115497842d4fa0c211bd54e6477
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 02:28:46 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Prints alphabet
+*
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+char[26] = "abcdefghijklmnopqrstuvwxyz";
+
+int i = 0;
+for (i = 0; i < 26; i++)
+{
+_putchar(char[i]);
+}
+_putchar("\n");
+}
+return (0);
