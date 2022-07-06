87bc3d9869f7304e0d65582d7c6347c82a37ffaa
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 04:27:50 -0700

    Print alphabet 10 times

---
+++ 0x02-functions_nested_loops/print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include main.h
+
+/**
+* main - Prints the alphabet 10 times
+*
+* Return: nothing
+*/
+
+void main(void)
+{
+char alp[26] = abcdefghijklmnopqrstuvwxyz;
+
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 26; j++)
+{
+_putchar(alp[j]);
+}
+_putchar('\n');
+}
+}
