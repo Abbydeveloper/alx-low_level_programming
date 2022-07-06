1302eff35b104e25d419de0f48731b34b7ca393f
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 05:23:38 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+* _isalpha - Check for alphabetic character
+* @c: character to check
+*
+* Return: 1 (Success)
+* On error, return 0
+
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
