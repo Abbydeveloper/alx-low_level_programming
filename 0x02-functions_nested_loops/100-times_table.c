fd49350c2cf5f7ad3ae6dcfe9192d37edff257d8
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 21:35:17 -0700

    Fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,11 @@
 #include "main.h"
-
 /**
 * print_times_table - prints the n times table
 * @n: number of the integer
-* Return: nothing
 */
-
 void print_times_table(int n)
 {
 int i, j, k;
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <=  n; i++)
@@ -42,11 +38,9 @@ else if (k >= 100)
 _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
+_putchar((k / 10) % 10) + '0');
+_putchar(((k % 10) + '0');
 }
 }
 _putchar('\n');
-}
-}
-}
+}}}
