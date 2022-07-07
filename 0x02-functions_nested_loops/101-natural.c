4abc1a56ddd795e74e415fc30a0bc466e09a21f5
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 21:46:32 -0700

    Fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 int start_num, end_num, total;
 
 end_num = 1024;
-totoal = 0;
+total = 0;
 
 for (start_num = 0; start_num < end_num; start_num++)
 {
-if ((start+num % 3 == 0) || (start_num % 5 == 0))
+if ((start_num % 3 == 0) || (start_num % 5 == 0))
 {
 total = total + start_num;
 }
@@ -24,7 +24,7 @@ else
 continue;
 }
 }
-printf('%d', total);
+printf("%d", total);
 printf('\n');
 
 return (0);
