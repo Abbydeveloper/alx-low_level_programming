49a792b264abfead56a749516cbe972bcf019391
Abigail Adeboga <abbyboga@gmail.com>
2022-07-06 21:43:00 -0700

    Natural number

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - sums up all multiples of 3 or 5
+*
+*Return: Always(0)
+*/
+
+int main(void)
+{
+int start_num, end_num, total;
+
+end_num = 1024;
+totoal = 0;
+
+for (start_num = 0; start_num < end_num; start_num++)
+{
+if ((start+num % 3 == 0) || (start_num % 5 == 0))
+{
+total = total + start_num;
+}
+else
+{
+continue;
+}
+}
+printf('%d', total);
+printf('\n');
+
+return (0);
+}
