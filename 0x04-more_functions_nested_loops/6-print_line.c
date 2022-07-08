8902ee25ff7c110814eb6a416749966840e288e6
Abigail Adeboga <abbyboga@gmail.com>
2022-07-08 06:50:48 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_line - print a straight line n number of times
+ * @n: type integer
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
