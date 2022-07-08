d174e689227dd620ce16eef6612cf0d2ea0e1911
Abigail Adeboga <abbyboga@gmail.com>
2022-07-08 03:24:18 -0700

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * _isdigit - check if character is a single digit number
+ * @c: digit to check
+ *
+ * Return: 0 if c is a digit
+ * otherwise return 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
