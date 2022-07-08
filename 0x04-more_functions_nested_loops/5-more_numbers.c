253e45ca4d97f3cac45cd323c54f5b50ba16d803
Abigail Adeboga <abbyboga@gmail.com>
2022-07-08 04:10:37 -0700

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,10 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <=14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			_putchar(i + '0');
-		}
-	
+		}	
 	_putchar('\n');
 
 	}
