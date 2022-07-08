ceaffe2367c59c7b909a6055eabe14ae17c0a716
Abigail Adeboga <abbyboga@gmail.com>
2022-07-08 07:19:05 -0700

    Print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draw a diagonal line on the terminal
+ * @n: number type integer
+ *
+ * Return: nothing
+ */
+
+int print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (j == i)
+			{
+				_putchar('\');
+			}
+			else {
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
