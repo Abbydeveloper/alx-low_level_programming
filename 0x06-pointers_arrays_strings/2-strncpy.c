0e3d1fc0745460a0981c0ffa60c13cc695903c58
Abigail Adeboga <abbyboga@gmail.com>
2022-07-15 21:29:27 -0700

    Copy strig

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: char dest
+ * @src: char src
+ * @n: int n
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+
+	}
+
+	return (dest);
+}
