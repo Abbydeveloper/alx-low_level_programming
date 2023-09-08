37fdc0876f8f813873315d0f1da5d1f35d6d8fe1
root <Adeboga Abigail>
2023-09-08 13:25:51 -0700

    Fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,8 @@
 /**
  * main - Print the size of various types on the computer
  *
- * Return: 0 on success
+ * Return: return 0 on success
  */
-
 int main(void)
 {
 	char a;
@@ -13,9 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int %lu bytes(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
