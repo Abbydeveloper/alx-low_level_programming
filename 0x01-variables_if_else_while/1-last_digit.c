2e0540ca40db699bb0d37944559b03a89b5a6e96
root <Adeboga Abigail>
2023-09-09 06:20:47 -0700

    Script to display last digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print last digit of number stored in n
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int r = n % 10;
+	char str = "Last digit of ";
+
+	if (r > 5)
+		printf("%s%d is %d and is greater than 5\n", str, n, r);
+	else if (r < 6 && r > 0)
+		printf("%s%d is %d and is less than 6 and not 0\n", str, n, r);
+	else if (r == 0)
+		printf("%s%d is %d and is 0\n", str, n, r);
+	return (0);
+}
