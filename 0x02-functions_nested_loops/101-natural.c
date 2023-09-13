6afdb00839bddb2317df645c01a0722bfe58cb55
root <Adeboga Abigail>
2023-09-13 13:50:29 -0700

    Program to print sum of all multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * natural - print the sum of all multiples of 3 or 5 below 1023
+ *
+ */
+
+void natural(void)
+{
+	int i, j;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			j += i;
+	}
+	printf("%d\n", j);
+}
