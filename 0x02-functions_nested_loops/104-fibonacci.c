c5d2045cf9031e48b5448050f4b280909991bb8d
root <Adeboga Abigail>
2023-09-13 14:54:34 -0700

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fibonacci numbers
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i = 0, limit = 98;
+	long int j = 1, k = 1;
+
+	while (i < limit)
+	{
+		long int n;
+
+		n = j + k;
+
+		if (i < limit - 1)
+			printf("%lu, ", k);
+		else
+			printf("%lu\n", k);
+		j = k;
+		k = n;
+
+		i++;
+	}
+	return (0);
+}
