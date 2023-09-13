caf32775f6ae2939feeb3cfe0e80c47ae3cb76af
root <Adeboga Abigail>
2023-09-13 15:52:37 -0700

    Fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -27,7 +27,7 @@ int main(void)
 	j1 = j / 1000000000;
 	j2 = j % 1000000000;
 
-	for (i = 91; i < limit; i++)
+	for (i = 91; i < limit; ++i)
 	{
 		unsigned long int n1, n2;
 
@@ -35,9 +35,9 @@ int main(void)
 		n2 = j2 + k2;
 
 		if (i < limit - 1)
-			printf("%lu, ", k1 + (k2 / 1000000000));
+			printf("%lu%lu, ", k1 + (k2 / 1000000000), (k2 % 1000000000));
 		else
-			printf("%lu\n", k1 + (k2 % 1000000000));
+			printf("%lu%lu\n", k1 + (k2 % 1000000000), (k2 % 1000000000));
 		j1 = k1;
 		j2 = k2;
 		k1 = n1;
