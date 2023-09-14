fb57290bb3a91c4c6a215c809ac381fa854c385e
root <Adeboga Abigail>
2023-09-14 08:55:49 -0700

    Fix 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
@@ -20,6 +20,9 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
+	return (0);
 }
