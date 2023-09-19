439e2dfb961a89de41c3d0ec8c1ad15a6c4d1b35
root <Adeboga Abigail>
2023-09-19 09:35:55 -0700

    C program to reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+		i++;
+	j = i;
+
+	for (j = i; j > 0; j--)
+		_putchar(s[j] + '\0');
+	_putchar('\n');
+}
