d43ceb46510e2825c99ed497bf7f0c5c85c2f81d
root <Adeboga Abigail>
2023-09-19 08:51:01 -0700

    C program to print a string followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string followed by a new line
+ * @str: String to print
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		_putchar(str[i]);
+	_putchar('\n');
+}
