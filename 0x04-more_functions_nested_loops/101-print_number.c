401368890d356dcc1347fd901e963e02936faa30
root <Adeboga Abigail>
2023-09-23 14:30:25 -0700

    Fix???

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,15 +19,12 @@ void print_number(int n)
 	}
 	if (n == 0)
 		_putchar('0');
-	if (n > INT_MAX / 10 || (n == INT_MAX / 10 && s[i] - '0' > 7))
-	{
-		if (neg_count == 1)
-			return (INT_MAX)
-		else
-			return (INT_MIN);
-	}
+
 	for (i = 1000000000; i >= 1; i /= 10)
 	{
+		if (n >= INT_MAX || n <= INT_MIN)
+			n = INT_MAX;
+		printf("%d\n", n);
 		if (n / i != 0)
 			_putchar((n / i) % 10 + '0');
 	}
