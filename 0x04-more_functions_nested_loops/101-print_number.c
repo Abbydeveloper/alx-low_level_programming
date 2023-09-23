a98542a835bf6c3d7851c92b57f17a175c7e3599
root <Adeboga Abigail>
2023-09-23 14:03:40 -0700

    Back to former solution

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,7 +18,7 @@ void print_number(int n)
 	}
 	if (n == 0)
 		_putchar('0');
-	for (i >= 1000000000; i >= 1; i /= 10)
+	for (i = 1000000000; i >= 1; i /= 10)
 	{
 		if (n / i != 0)
 			_putchar((n / i) % 10 + '0');
