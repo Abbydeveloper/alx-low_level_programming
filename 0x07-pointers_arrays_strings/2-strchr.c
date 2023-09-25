05de13b0c228782edab1f1651c4b5ca5b2dd9132
root <Adeboga Abigail>
2023-09-25 16:32:31 -0700

    Replicate strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - locate character in a string
+ * @c: character to locate
+ * @s: string to seach
+ * Return: pointer to the first occurrence of c
+ * or NULL if it is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return ((s + i));
+		}
+	}
+	return (NULL);
+}
