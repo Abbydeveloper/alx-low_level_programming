8095e4cae35ad2f73ec2a368f67d2084be484bfe
Adeboga Abigail <abbyboga@gmail.com>
2023-10-02 14:00:20 -0700

    Program to get square root of natural number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt - run binary search to find square root
+ * @start: number to start search from
+ * @end: number to end search at
+ * @mid: middle number
+ *
+ * Return: square root
+ */
+
+int _sqrt(int start, int end, int n)
+{
+	int mid;
+
+	if (start <= end)
+	{
+		mid = (start + end) / 2;
+
+		if ((mid * mid == n) &&
+				((mid + 1) * (mid + 1) > n))
+			return mid;
+		else if (mid * mid < n)
+			return (_sqrt(mid + 1, end, n));
+		else
+			return (_sqrt(start, mid - 1, n));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number to return square root of
+ *
+ * Return: square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int start = 1, end = n;
+
+	return (_sqrt(start, end, n));
+}
