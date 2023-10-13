d2bbdeff27790059cc3097d25ce0cfbd9677f6da
Adeboga Abigail <abbyboga@gmail.com>
2023-10-13 13:27:59 -0700

    Fix errors

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p, i;
+	char *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -21,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; p[i] != '\0'; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 
 	p[i] = '\0';
