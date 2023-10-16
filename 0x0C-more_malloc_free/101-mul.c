a3a2497c0001f091a9b3eb588636facacff293e7
Adeboga Abigail <abbyboga@gmail.com>
2023-10-16 13:44:04 -0700

    Mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - multiply two positive numbers
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: result of multiplication
+ */
+
+int main(int ac, char **av)
+{
+	unsigned long mul;
+	int i, j;
+
+	if (ac < 3 || ac > 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			if (av[i][j] < 48 || av[i][j] > 57)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	mul = atoi(av[1]) * atoi(av[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
