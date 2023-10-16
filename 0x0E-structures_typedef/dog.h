13565267c32b4ef22e42803190d3954237ec0694
Adeboga Abigail <abbyboga@gmail.com>
2023-10-16 05:40:22 -0700

    Fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,31 +3,18 @@
 #include <stdlib.h>
 
 /**
- * struct dog - 
+ * struct dog - declare dog struct
  * @name: name of dog
  * @age: age of dog
  * @owner: dog owner
  *
  */
 
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-struct Dog *my_dog(char *name, float age, char *owner)
-{
-	struct Dog *my_dog;
-
-	my_dog = malloc(sizeof(struct Dog));
-	if (my_dog == NULL)
-		return (NULL);
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-	return my_dog;
-}
 
 #endif
