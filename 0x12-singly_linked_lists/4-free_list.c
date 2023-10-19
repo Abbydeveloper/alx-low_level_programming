45f8fa50129689d6a78a5ff309fe627f742f89cf
Adeboga Abigail <abbyboga@gmail.com>
2023-10-19 15:15:01 -0700

    Fix all errors

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,11 +10,10 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while(head)
+	while (head)
 	{
 		temp = head->next;
 		free(head->str);
-		free(head->next);
 		free(head);
 		head = temp;
 	}
