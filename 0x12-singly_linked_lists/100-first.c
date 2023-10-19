4c893643e2dc97bd799f851fbc5795c8768137d4
Adeboga Abigail <abbyboga@gmail.com>
2023-10-19 15:38:19 -0700

    Fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void before_main(void) __attribute__((constructor));
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you muct allow,");
+	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
 }
