fae964d738e770b5001f70aa24925e3045ab0fc6
Adeboga Abigail <abbyboga@gmail.com>
2023-10-19 15:10:39 -0700

    Free list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free a list_t list
+ * @head: head node of list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while(head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head->next);
+		free(head);
+		head = temp;
+	}
+}
