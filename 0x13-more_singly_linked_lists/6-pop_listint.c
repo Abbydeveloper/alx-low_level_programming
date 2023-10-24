f32cedb1cdcfbb6419c052fe87db27037b43db82
Adeboga Abigail <abbyboga@gmail.com>
2023-10-24 15:36:46 -0700

    Fix betty error

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,6 +6,9 @@
  * pop_listint - delete head node of a listint_t linked list
  * and return the head node's data (n)
  * @head: head node
+ *
+ * Return: the head node's data (n)
+ * or 0 if the linked list is empty
  */
 
 int pop_listint(listint_t **head)
