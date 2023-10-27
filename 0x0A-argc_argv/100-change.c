1a1cf3e9cfe48f8685effa224b0be05457b5a880
Abbydeveloper <101725228+Abbydeveloper@users.noreply.github.com>
2023-10-27 11:21:46 +0100

    Argument count, and argument vector

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print minimum number of conins to make change
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0, cents;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			i = cents / 25;
+			cents = cents % 25;
+		}
+		else if (cents >= 10)
+		{
+			i += cents / 10;
+			cents = cents % 10;
+		}
+		else if (cents >= 5)
+		{
+			i += cents / 5;
+			cents = cents % 5;
+		}
+		else if (cents >= 2)
+		{
+			i += cents / 2;
+			cents = cents % 2;
+		}
+		else if (cents >= 1)
+		{
+			i += cents / 1;
+			cents = 0;
+		}
+	}
+	printf("%d\n", i);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _isNumber - check if a string contains only digits
+ * @s: string to check
+ *
+ * Return: 1 if string is a number,
+ * 0 if not
+ */
+
+int _isNumber(char s[])
+{
+	int i = 0;
+
+	if (s[0] == '-')
+		i = 1;
+
+	while (s[i] != '\0')
+	{
+		if (isdigit(s[i]))
+			i++;
+		else
+			return (0);
+	}
+
+	return (1);
+}
+/**
+ * main - add positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if successful
+ * otherwise, 1
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc >= 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (_isNumber(argv[i]))
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
