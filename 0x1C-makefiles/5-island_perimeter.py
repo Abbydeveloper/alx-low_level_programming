be2a4f6823ff3dab63fbccc0e35d7b1994728762
Adeboga Abigail <abbyboga@gmail.com>
2024-03-25 09:36:40 -0700

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Define a function to measure the perimeter of an island"""
+
+def island_perimeter(grid):
+    """
+    Return island perimeter
+
+    0 is a water zone
+    1 is a land zone
+
+    Args:
+        grid: a list of list of integers
+    Returns:
+        Island perimeter
+    """
+
+    edge = 0
+    size = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size++
+                if (j > 0 and grid[i][j - 1] == 1) or
+                    (i > 0 and grid[i - 1][j] == 1):
+                    edge++
+
+    return ((size * 4) - (edge * 2))
