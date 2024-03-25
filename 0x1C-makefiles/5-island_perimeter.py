1816dc69a42a29c6e78b68fdc7cefc2d1bc3d174
Adeboga Abigail <abbyboga@gmail.com>
2024-03-25 10:44:49 -0700

    Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,9 +20,10 @@ def island_perimeter(grid):
     for i in range(height):
         for j in range(width):
             if grid[i][j] == 1:
-                size++
-                if (j > 0 and grid[i][j - 1] == 1)
-                or (i > 0 and grid[i - 1][j] == 1):
-                    edge++
+                size += 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    edge += 1
+                if i > 0 and grid[i - 1][j] == 1:
+                    edge += 1
 
     return ((size * 4) - (edge * 2))
