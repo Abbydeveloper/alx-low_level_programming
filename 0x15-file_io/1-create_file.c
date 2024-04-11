9f5796a9f375b163ad2d71764d7b86a0446b3304
Adeboga Abigail <abbyboga@gmail.com>
2024-04-11 00:27:40 -0700

    Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,14 @@
 int create_file(const char *filename, char *text_content)
 {
 	int open_file, write_file, length;
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 	{
 		length = 0;
-	} else {
+	} else
+	{
 		for (length = 0; text_content[length];)
 		{
 			length++;
