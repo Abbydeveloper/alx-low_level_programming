31b73b55504ad6ab48e5a0d744ae5d6b39f26022
Adeboga Abigail <abbyboga@gmail.com>
2024-05-09 14:26:55 -0700

    linear search errors fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,7 +11,7 @@
  * else return first index where value is located
  */
 
-int linear_search(int *array, size_t size, int value):
+int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
@@ -20,8 +20,9 @@ int linear_search(int *array, size_t size, int value):
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]", i, array[i]);
-		return (i);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
